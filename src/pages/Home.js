e364510407d54129332b62687f50a6d32bb12f10
tirthp14 <tirth.patel1721@gmail.com>
2024-06-29 21:38:30 -0400

    Slightly edited home page and index.html

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import SelectRace from "../components/SelectRace";
 import FastestLap from "../components/FastestLap";
 import FastestPitstop from "../components/FastestPitstop";
 import WindData from "../components/WindData";
+import StartingGrid from "../components/StartingGrid";
 import RaceResults from "../components/RaceResults";
 
 function Home() {
@@ -13,6 +14,7 @@ function Home() {
         <FastestLap/>
         <FastestPitstop/>
         <WindData/>
+        <StartingGrid/>
         <RaceResults/>
       </div>
     </SessionProvider>
