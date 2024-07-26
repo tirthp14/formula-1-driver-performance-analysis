eb815316e6afa71659ba317916e0d95e0a54173d
tirthp14 <tirth.patel1721@gmail.com>
2024-07-25 21:30:23 -0400

    Updated RaceTrack.js page

--- src/components/RaceTrack.js
+++ src/components/RaceTrack.js
@@ -3,22 +3,65 @@ import { SessionContext } from "../utils/SessionContext";
 
 const RaceTrack = ({lapNumber, driverNumber}) => {
     const { qualifyingSessionKey } = useContext(SessionContext);
+    const [lapData, setLapData] = useState(null);
+    const [trackData, setTrackData] = useState(null);
 
     useEffect(() => {
-        console.log("The lap number selected was: ", lapNumber)
-        console.log("The driver number selected was: ", driverNumber)
         const fetchTrackCoordinates = async () => {
-            try {
-                const response = await fetch(`https://api.openf1.org/v1/location?session_key=${qualifyingSessionKey}`);
-                const data = await response.json();
+            if (qualifyingSessionKey && driverNumber && lapNumber) {
+                try {
+                    const response = await fetch(`https://api.openf1.org/v1/laps?session_key=${qualifyingSessionKey}&driver_number=${driverNumber}&lap_number=${lapNumber}`);
+                    const lapData = await response.json();
 
-            } catch (error) {
-                console.log("Error fetching location data: ", error);
+                    setLapData(lapData)    
+                } catch (error) {
+                    console.log("Error fetching location data: ", error);
+                }
             }
         }
 
         fetchTrackCoordinates();
-    }, [lapNumber])
+    }, [qualifyingSessionKey, driverNumber, lapNumber]);
+
+    useEffect(() => {
+        if (lapData) {
+            processData(lapData[0].date_start, lapData[0].lap_duration);
+        }
+
+    }, [lapData]);
+
+    const fetchTrackData = async (qualifyingSessionKey, driverNumber) => {
+        const response = await fetch(`https://api.openf1.org/v1/location?session_key=${qualifyingSessionKey}&driver_number=${driverNumber}`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    const processData = async (dateStart, lapDuration) => {
+        try {
+            const data = await fetchTrackData(qualifyingSessionKey, driverNumber)
+
+            const startTime = new Date(dateStart).getTime();
+            const endTime = startTime + lapDuration * 1000;
+
+            const startIndex = findClosestIndex(data, startTime);
+            const endIndex = findClosestIndex(data, endTime);
+
+            setTrackData(data.slice(startIndex, endIndex + 1));
+
+            console.log(trackData)
+        } catch (error) {
+            console.log("Error fetching Graph Data: ", error);
+        }
+    }
+    
+    const findClosestIndex = (data, targetTime) => {
+        return data.reduce((closestIndex, entry, index) => {
+            const entryTime = new Date(entry.date).getTime();
+            const diff = Math.abs(entryTime - targetTime);
+            return diff < Math.abs(new Date(data[closestIndex].date).getTime() - targetTime) ? index : closestIndex;
+        }, 0)
+    }
 }
 
 export default RaceTrack;
