47552a65c999de46dfbc65b255b9e1560e28958a
tirthp14 <tirth.patel1721@gmail.com>
2024-08-14 22:13:57 -0400

    Edited navbar section

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav className="bg-[#ffffff] bg-opacity-0 md:bg-opacity-20 backdrop-blur-sm text-white h-14 w-full">
-            <Link to="/">Home</Link>
-            <Link to="race-analysis">Race Analysis</Link>
-            <Link to="qualifying-analysis">Qualifying Analysis</Link>
-        </nav>
+        <div>
+            <nav className="bg-[#ffffff] bg-opacity-0 md:bg-opacity-20 backdrop-blur-sm text-white h-14 w-full">
+                <Link to="/">HOME</Link>
+                <Link to="race-analysis">RACE ANALYSIS</Link>
+                <Link to="qualifying-analysis">QUALIFYING ANALYSIS</Link>
+            </nav>
+        </div>
     );
   }
   
