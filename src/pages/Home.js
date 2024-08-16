14e803258d08bb408ee7f72dc2e13dc23546380a
tirthp14 <tirth.patel1721@gmail.com>
2024-08-15 22:45:49 -0400

    Edited the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div className="bg-mainBackground text-white">
-      <h2>Welcome to F1 Driver Performance Analysis</h2>
+      <h2>Welcome to F1 Driver Performance Analysis.</h2>
       <p>This website helps with race analysis by looking at lap times, car data, and more.</p>
       <p>Use the navigation bar to explore race analysis and qualifying analysis features on the page.</p>
     </div>
